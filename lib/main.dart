d773ec37affeb1f317afd61a19fc9751692ac9e3
Anurag Sood <anurag.sood007@gmail.com>
2019-07-03 14:06:55 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,78 +9,81 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey[900],
+        backgroundColor: Colors.amber.shade500,
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/colt.jpg'),
-              ),
-              Text(
-                'Colt Carson',
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 40.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/chim.jpeg'),
                 ),
-              ),
-              Text(
-                'LEAD DATA SCIENTIST',
-                style: TextStyle(
-                  fontFamily: 'Source Sans Pro',
-                  color: Colors.white,
-                  fontSize: 20.0,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 150.0,
-                child: Divider(
-                  color: Colors.teal.shade100,
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.blueGrey[900],
+                Text(
+                  'Chimchar',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
                   ),
-                  title: Text(
-                    '727-282-3314',
-                    style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 20.0,
+                ),
+                Text(
+                  'FIRE-TYPE POKEMON',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 17.0,
+                    letterSpacing: 2.5,
+                    color: Colors.amber.shade100,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.amber.shade50,
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.check,
+                      color: Colors.amber,
+                    ),
+                    title: Text(
+                      'FIRE BLAST',
+                      style: TextStyle(
+                        color: Colors.amber.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.email,
-                    color: Colors.blueGrey[900],
-                  ),
-                  title: Text(
-                    'colt_carson@live.com',
-                    style: TextStyle(
-                      color: Colors.teal.shade900,
-                      fontFamily: 'Source Sans Pro',
-                      fontSize: 20.0,
-                    ),
-                  ),
-                ),
-              ),
-            ],
+                Card(
+                    color: Colors.white,
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.location_city,
+                        color: Colors.amber,
+                      ),
+                      title: Text(
+                        'HOEEN REGION',
+                        style: TextStyle(
+                          color: Colors.amber.shade900,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    )),
+              ],
+            ),
           ),
         ),
       ),
